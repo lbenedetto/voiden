e462129cca02e094b8ae38679583eae4edd21cb1
phurpa-tsering <phurpa@apyhub.com>
2026-03-25 16:49:10 +0530

    dev : changelog icon set

--- apps/ui/src/core/screens/ChangeLogScreen.tsx
+++ apps/ui/src/core/screens/ChangeLogScreen.tsx
@@ -1,5 +1,5 @@
 import { Cookie, FileText, Puzzle } from "lucide-react";
-import { Terminal, Plug, FileDown, Paintbrush, Braces, Code, LayoutDashboard, Eye, FilePlus2, Rocket, Wrench, Zap, Sparkles, Compass } from "lucide-react";
+import { Terminal, Plug, FileDown, Paintbrush, Braces, Code, LayoutDashboard, Eye, FilePlus2, Rocket, Wrench, Zap, Sparkles, Compass, Workflow, PanelBottom } from "lucide-react";
 import changelogData from "@/data/changelog.json";
 
 // Map icon names to Lucide icon components
@@ -19,7 +19,9 @@ const iconMap = {
   Sparkles: Sparkles,
   Compass: Compass,
   Puzzle:Puzzle,
-  Cookie:Cookie
+  Cookie:Cookie,
+  Workflow:Workflow,
+  PanelBottom:PanelBottom
 };
 
 const ChangeLogScreen = () => {
